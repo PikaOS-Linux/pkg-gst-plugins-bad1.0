5d58f8079ea498cf16d94e6394bf66cf01d5a4e0
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-04-30 01:18:29 +0300

    vaapi av1

---
+++ main32.sh
@@ -0,0 +1,25 @@
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-bad1.0/gst-plugins-bad1.0_1.22.1.orig.tar.xz
+mkdir -p ./tar-arc
+tar -xf ./gst-plugins-bad1.0_1.22.1-1ubuntu1.orig.tar.xz -C ./tar-arc/
+mv ./tar-arc/* ./gst-plugins-bad1.0
+cp -rvf ./debian ./gst-plugins-bad1.0/
+cd ./gst-plugins-bad1.0
+
+# Get build deps
+apt-get install build-essential -y
+apt-get install crossbuild-essential-i386 lib32gcc-11-dev -y
+apt-get build-dep ./ -y -a i386
+
+# Build package
+dpkg-buildpackage -a i386
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
