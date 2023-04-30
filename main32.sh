cec45b513ed87fafaa5536272224fa0e0f3db22c
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-04-30 22:36:13 +0300

    Update main32.sh

--- main32.sh
+++ main32.sh
@@ -18,7 +18,7 @@ apt-get install -y pbuilder debootstrap devscripts debhelper
 
 # Build package
 apt-get build-dep -y ./
-debuild -S -sa -b
+debuild -S -sa -uc -us
 
 DIST=lunar ARCH=i386 pbuilder create
 DIST=lunar ARCH=i386 pbuilder build ./*.dsc
