29bb56dfd8cf2cfe625fc96ef928dc1dbda1c58e
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-01 01:24:32 +0300

    Update main32.sh

--- main32.sh
+++ main32.sh
@@ -16,17 +16,16 @@ cd ./gst-plugins-bad1.0
 
 apt-get install -y pbuilder debootstrap devscripts debhelper sbuild debhelper ubuntu-dev-tools piuparts
 
-# Build package
-apt-get build-dep -y ./
-debuild -S -uc -us
-cd ../
-ls
-echo 'phase1'
-touch ~/.sbuildrc
-mk-sbuild lunar --arch=i386
-echo 'phase2'
-sbuild -d lunar-i386 ./*.dsc
+ARG USERNAME=gitboi
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
 
-# Move the debs to output
-mkdir -p ./output
-mv ./*.deb ./output/
+CUR_DIR=$(pwd)
+# Create the user
+groupadd --gid $USER_GID $USERNAME \
+    && useradd --uid $USER_UID --gid $USER_GID -m $USERNAME \
+    && apt-get update \
+    && apt-get install -y sudo \
+    && echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
+    && chmod 0440 /etc/sudoers.d/$USERNAME
+su $USERNAME -c $CUR_DIR/sbuild32.sh
