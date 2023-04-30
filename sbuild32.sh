dc62bf9eb32d5fd88dada6ff8ee7fa20742b2acf
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-01 01:17:56 +0300

    Update sbuild32.sh

--- sbuild32.sh
+++ sbuild32.sh
@@ -1,25 +1,3 @@
-# Add dependent repositories
-sudo dpkg --add-architecture i386
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-# Clone Upstream
-wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-bad1.0/gst-plugins-bad1.0_1.22.1.orig.tar.xz
-mkdir -p ./tar-arc
-tar -xf ./gst-plugins-bad1.0_1.22.1-1ubuntu1.orig.tar.xz -C ./tar-arc/
-mv ./tar-arc/* ./gst-plugins-bad1.0
-cp -rvf ./debian ./gst-plugins-bad1.0/
-cd ./gst-plugins-bad1.0
-
-# Get build deps
-
-apt-get install -y pbuilder debootstrap devscripts debhelper sbuild debhelper ubuntu-dev-tools piuparts
-
-# Build package
-apt-get build-dep -y ./
-debuild -S -uc -us
-cd ../
 
 echo 'phase1'
 touch ~/.sbuildrc
@@ -27,6 +5,5 @@ mk-sbuild lunar --arch=i386
 echo 'phase2'
 sbuild -d lunar-i386 ./*.dsc
 
-# Move the debs to output
-mkdir -p ./output
+# Move the debs to outputmkdir -p ./output
 mv ./*.deb ./output/
