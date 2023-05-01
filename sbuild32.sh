4665404c445d9694351d54b48c9aa4e0bf50cc21
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-01 15:22:46 +0300

    Update sbuild32.sh

--- sbuild32.sh
+++ sbuild32.sh
@@ -1,9 +1,7 @@
-
-echo 'phase1'
-
-mk-sbuild sid --arch=i386 --debootstrap-include=libeatmydata1 --debootstrap-mirror=http://cdn-fastly.deb.debian.org/debian
-echo 'phase2'
-sbuild -d sid-i386 ./*.dsc
+sudo rm -rf /var/cache/apt/
+DIST=sid ARCH=i386 pbuilder create --distribution sid --architecture i386
+echo 'starting build'
+DIST=sid ARCH=i386 pbuilder build ./*.dsc --distribution sid --architecture i386
 
 # Move the debs to outputmkdir -p ./output
 sudo mv ./*.deb ./output/
