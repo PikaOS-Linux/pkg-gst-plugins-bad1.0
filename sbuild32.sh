8765900ee9c413faaca64c0280533e84e6e1feef
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-01 15:25:46 +0300

    Update sbuild32.sh

--- sbuild32.sh
+++ sbuild32.sh
@@ -1,7 +1,7 @@
 sudo rm -rf /var/cache/apt/
-DIST=sid ARCH=i386 pbuilder create --distribution sid --architecture i386
+DIST=sid ARCH=i386 pbuilder create --mirror http://deb.debian.org/debian --distribution sid --architecture i386
 echo 'starting build'
-DIST=sid ARCH=i386 pbuilder build ./*.dsc --distribution sid --architecture i386
+DIST=sid ARCH=i386 pbuilder build ./*.dsc --mirror http://deb.debian.org/debian  --distribution sid --architecture i386
 
 # Move the debs to outputmkdir -p ./output
 sudo mv ./*.deb ./output/
