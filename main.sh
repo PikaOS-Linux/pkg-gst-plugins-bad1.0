f1ccc71c99fe0246f7e9ed40bc9ec0d6b78b1a0e
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-02 01:01:20 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,9 @@
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 touch /etc/apt/sources.list.d/pika.list
 echo 'deb https://ppa.pika-os.com/ lunar main' > /etc/apt/sources.list.d/pika.list
+echo 'deb http://deb.debian.org/debian/ sid main contrib non-free non-free-firmware' > /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian/ sid main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+apt update
 #add-apt-repository ppa:pikaos/pika
 #add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
