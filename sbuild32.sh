11908de601c8d2fe8ecb29187491cedd36eb2e36
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-01 15:47:52 +0300

    Update sbuild32.sh

--- sbuild32.sh
+++ sbuild32.sh
@@ -1,8 +1,8 @@
 sudo apt install -y debian-archive-keyring
 sudo rm -rf /var/cache/apt/
-sudo ARCH=i386 pbuilder create --architecture i386
+sudo ARCH=i386 pbuilder create --mirror http://http.us.debian.org/debian --distribution sid --architecture i386
 echo 'starting build'
-sudo ARCH=i386 pbuilder build ./*.dsc --architecture i386
+sudo ARCH=i386 pbuilder build ./*.dsc --mirror http://http.us.debian.org/debian --distribution sid --architecture i386
 
 # Move the debs to outputmkdir -p ./output
 sudo mv ./*.deb ./output/
