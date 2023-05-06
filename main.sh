f9faaee6325b8c468389ce02cda8274339fb0847
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-06 21:46:40 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-#touch /etc/apt/sources.list.d/pika.list
-#echo 'deb https://ppa.pika-os.com/ lunar main' > /etc/apt/sources.list.d/pika.list
+touch /etc/apt/sources.list.d/pika.list
+echo 'deb https://ppa.pika-os.com/ lunar main' > /etc/apt/sources.list.d/pika.list
 #echo 'deb http://deb.debian.org/debian/ sid main contrib non-free non-free-firmware' > /etc/apt/sources.list
 #echo 'deb-src http://deb.debian.org/debian/ sid main contrib non-free non-free-firmware' >> /etc/apt/sources.list
 #apt update
